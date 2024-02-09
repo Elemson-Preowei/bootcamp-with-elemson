817dc8b63e3c72bdf402c686190ad26347a8d90a
Elemson Preowei <elemsonp@gmail.com>
2024-02-09 16:42:34 +0100

    assignment submission

--- solutions/06-jan.py
+++ solutions/06-jan.py
@@ -12,20 +12,27 @@ of each type.
 class Flower:
     def __init__(self, name, petals, price):
         self.name = name
-        self.petals = 0.0
-        self.price = 0 
+        self.petals = 0
+        self.price = 0.0 
 
+    def set_name(self, name):
+        self.name = name
+    
     def get_name(self):
         return self.name
     
     def set_petals(self, num):
-        return self.petals += num
+        self.petals = num
 
     def get_petals(self):
         return self.petals
     
     def set_price(self, amt):
-        return self.price += amt
+        self.price = amt
 
     def get_price(self):
         return self.price
+
+habiscus = Flower("habiscus", 5, 1000.00)
+habiscus.set_name("rose")
+print(f"The flower on sale is {habiscus.get_name()}")
